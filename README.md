d67155d73c5803f27b763a9120c39a233f7938bc
otk <ozren.krznaric@gmail.com>
2022-05-11 18:12:16 +0200

    Load titanic ds + RandomForestClassifier works

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ml-tests
 ML tests
+
+# Virtual env
+`python -m venv ./venv`
+
+# Install pip packages
+`pip install -r requirements.txt`
--- ml-test-01.py
+++ ml-test-01.py
@@ -1,12 +1,51 @@
-from sklearn import datasets
-from sklearn.datasets import fetch_openml
-from joblib import Memory
-import time
+import numpy as np 
+import pandas as pd 
 
-time0 = time.time()
+from sklearn import linear_model
+from sklearn import tree
+from sklearn.datasets import fetch_openml
+from sklearn.model_selection import StratifiedKFold, train_test_split
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.metrics import accuracy_score
+
+from joblib import Memory
+
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
 memory = Memory('./tmp')
 fetch_openml_cached = memory.cache(fetch_openml)
-X, y = fetch_openml_cached("titanic", version=1, as_frame=True, return_X_y=True)
-print('Time: {:.0f}s'.format(time.time() - time0))
+bunch = fetch_openml_cached("titanic", version=1, as_frame=True)
+# print(f"DataFrame shape : {bunch.frame.shape}\n=================================")
+# print(f"DataFrame info : {bunch.frame.info()}\n=================================")
+# print(f"DataFrame columns : {bunch.frame.columns}\n=================================")
+# print(f"The type of each column : {bunch.frame.dtypes}\n=================================")
+# print(f"How much missing value in every column : {bunch.frame.isna().sum()}\n=================================")
 
-print(X.to_string())
+# Drop rows with missing values
+bunch.frame = bunch.frame.dropna(subset=['age','sibsp','parch','fare'])
+#print(bunch.frame.to_string())
+
+# Form data (X) and target (y) frame
+X = bunch.frame[['age','sibsp','parch','fare']]
+y = bunch.frame[['survived']]
+# print(X.to_string())
+# print(y.to_string())
+
+# Split training / validation
+X_t, X_v, y_t, y_v = train_test_split(X, y, test_size=0.2, random_state=19761210)
+# print(X_t.to_string())
+# print(y_t.to_string())
+# print(X_v.to_string())
+# print(y_v.to_string())
+
+# Create classifier
+#model = tree.DecisionTreeClassifier(random_state=1, max_depth=2)
+model = RandomForestClassifier(random_state=10197612)
+
+# Train model
+model.fit(X, y)
+
+# See if prediction fits
+score = accuracy_score(y_t, model.predict(X_t[['age','sibsp','parch','fare']]))
+print("Accuracy score: ", score)
